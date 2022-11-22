32c4363624bcb2ddd7fc9734d5e545a24c8c14c8
Simone Codarin <codarinsimone@gmail.com>
2022-11-22 19:18:05 +0100

    Modifiche app
    Aggiunto il caricamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,62 @@
-document.addEventListener('DOMContentLoaded', function(){
-    let searchBtn = document.querySelector('#btnGet');
+document.addEventListener('DOMContentLoaded', () => {
+    let searchBtn = document.querySelector('#search');
     let cityInp = document.querySelector('#unsernameGet');
     let information = document.querySelector('#information');
     let inputBox = document.querySelector('form');
+    let spinnerElement = document.querySelector('.loader');
+    let isLoading = false;
 
-    searchBtn.addEventListener('click', () => {
-        let cityName = cityInp.value;
-        let cityNameToLowerCase = cityName.toLowerCase();
-        let finalURL = `https://api.teleport.org/api/urban_areas/slug:${cityNameToLowerCase}/scores/`;
+    spinnerElement.style.display='none';
+
+    const createCustomList = (type,text,color,id) => {
+        let e = document.createElement(type);
+        e.innerHTML = text;
+        e.style.color = color;
+        e.setAttribute('id',id);
+        information.appendChild(e);
+    }
+
+    const toggleSpinner = () => {
+        isLoading = !isLoading
+
+        if(isLoading){
+            spinnerElement.style.display='block';
+        }else{
+            spinnerElement.style.display='none';
+        }
+    }
+
+    inputBox.addEventListener("keypress", e => {
+        if(e.key === "Enter"){
+            e.preventDefault();
+            startSearch()
+        }
+    })
+
+    searchBtn.addEventListener('click', async () => startSearch());
+
+    const startSearch = async () => {
+
+        let responseCityInfo;
+
+        let cityName = cityInp.value.toLowerCase();
+        let finalURL = `https://api.teleport.org/api/urban_areas/slug:${cityName}/scores/`;
         inputBox.style.display='none';
         information.style.display = 'block';
 
-        fetch(finalURL)
-        .then((response) => response.json())
-        .then((data) => {
-            const createCustomList = (type,text,color) => {
-                let e = document.createElement(type);
-                e.textContent = text;
-                e.style.color = color;
-                information.appendChild(e);
-            }
-            const createCustomSummary = (type,text) =>{
-                let s = document.createElement(type);
-                s.innerHTML = text;
-                information.appendChild(s);
-            }
-            createCustomSummary('h2', cityName.toUpperCase()+":");
-            for(let i = 0; i<=data.categories.length-1; i++){
-                createCustomList('h3',data.categories[i].name,data.categories[i].color);
-                createCustomList('h4',"Score out of 10" + data.categories[i].score_out_of_10.toFixed(3),data.categories[i].color);
-            }
-            createCustomSummary('p', `<b>Summary: </b>` + data.summary);
-            createCustomSummary('p', `<b>Teleport city score: </b>` + data.teleport_city_score.toFixed(3));
+        
+        
+        try{
+            toggleSpinner();
+ 
+            const fetchInfo = await fetch(finalURL);
+            responseCityInfo = await fetchInfo.json();
+            console.log(responseCityInfo);
 
-            let reset = document.createElement('button');
-            reset.textContent = 'Search another city';
-            reset.setAttribute('id', 'reload')
-            information.appendChild(reset);
+        }catch(e){
+            toggleSpinner();
+            console.log(e)
 
-            reset.addEventListener('click',  function(){
-                location.reload(true);
-            })
-         }).catch(() => {
             if(cityName.length == 0){
                 inputBox.style.display='block';
                 information.style.display = 'block';
@@ -51,8 +66,25 @@ document.addEventListener('DOMContentLoaded', function(){
                 alert('The name of the city is not valid.');
                 location.reload(true);
             }
-        });
-    });
+
+        }finally{
+            toggleSpinner();
+        }
+
+        createCustomList('h2', cityName.toUpperCase()+":",null,null);
+        for(let i = 0; i <= responseCityInfo.categories.length-1; i++){
+            createCustomList('h3',responseCityInfo.categories[i].name + ": ",responseCityInfo.categories[i].color,null);
+            createCustomList('h4',"Score out of 10" + responseCityInfo.categories[i].score_out_of_10.toFixed(2),responseCityInfo.categories[i].color,null);
+        }
+        createCustomList('p', `<b>Summary: </b>` + responseCityInfo.summary,null,null);
+        createCustomList('p', `<b>Teleport city score: </b>` + responseCityInfo.teleport_city_score.toFixed(2),null,null);
+        createCustomList('button','Search another city',null,'reload');
+
+        document.querySelector('#reload').addEventListener('click', () => {
+            location.reload(true);
+        })
+
+    }
 });
 
 
